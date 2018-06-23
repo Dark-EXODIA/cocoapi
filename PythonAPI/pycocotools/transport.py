34ac0d4d7b1b4bc422ac359ea3351301226f63e0
DarkMagician <omermagdy100@gmail.com>
2018-06-23 07:18:26 +0200

    Add files via upload

--- PythonAPI/pycocotools/transport.py
+++ PythonAPI/pycocotools/transport.py
@@ -3,13 +3,95 @@ from coco import COCO
 import os
 
 annFile='%s/annotations/instances_%s.json'%("/content/darkflow/train","train2014")
-
+labelnames=[""
+,"person"
+,"bicycle"
+,"car"
+,"motorbike"
+,"aeroplane"
+,"bus"
+,"train"
+,"truck"
+,"boat"
+,"traffic light"
+,"fire hydrant"
+,"stop sign"
+,"parking meter"
+,"bench"
+,"bird"
+,"cat"
+,"dog"
+,"horse"
+,"sheep"
+,"cow"
+,"elephant"
+,"bear""
+,"zebra"
+,"giraffe"
+,"backpack"
+,"umbrella"
+,"handbag"
+,"tie"
+,"suitcase"
+,"frisbee"
+,"skis"
+,"snowboard"
+,"sports ball"
+,"kite"
+,"baseball bat"
+,"baseball glove"
+,"skateboard"
+,"surfboard"
+,"tennis racket"
+,"bottle"
+,"wine glass"
+,"cup"
+,"fork"
+,"knife"
+,"spoon"
+,"bowl"
+,"banana"
+,"apple"
+,"sandwich"
+,"orange"
+,"broccoli"
+,"carrot"
+,"hot dog"
+,"pizza"
+,"donut"
+,"cake"
+,"chair"
+,"sofa"
+,"pottedplant"
+,"bed"
+,"diningtable"
+,"toilet"
+,"tvmonitor"
+,"laptop"
+,"mouse"
+,"remote"
+,"keyboard"
+,"cell phone"
+,"microwave"
+,"oven"
+,"toaster"
+,"sink"
+,"refrigerator"
+,"book"
+,"clock"
+,"vase"
+,"scissors"
+,"teddy bear"
+,"hair drier"
+,"toothbrush"]
+neededlabels=["person","dog","car","bicycle"]
 coco=COCO(annFile)
 cats = coco.loadCats(coco.getCatIds())
 nms=[cat['name'] for cat in cats]
 
 imgIds = coco.getImgIds()
 
+takeXml=0
 directory = './annotations_pascalformat/'
 if not os.path.exists(directory):
     os.makedirs(directory)
@@ -25,15 +107,19 @@ for n in range(len(imgIds)):
     xml += '<depth>\n3\n</depth>\n</size>\n<segmented>\n0\n</segmented>\n'
 
     for i in range(len(anns)):
-        bbox = anns[i]['bbox']
-        xml += '<object>\n<name>' + str(anns[i]['category_id']) + '</name>\n'
-        xml += '<bndbox>\n<xmin>\n' + str(int(round(bbox[0]))) + '\n</xmin>\n'
-        xml += '<ymin>\n' + str(int(round(bbox[1]))) + '\n</ymin>\n'
-        xml += '<xmax>\n' + str(int(round(bbox[0] + bbox[2]))) + '\n</xmax>\n'
-        xml += '<ymax>\n' + str(int(round(bbox[1] + bbox[3]))) + '\n</ymax>\n</bndbox>\n'
-        xml += '<truncated>\n0\n</truncated>\n<difficult>\n0\n</difficult>\n</object>\n'
+        if (labelnames[anns[i]['category_id']] in neededlabels):
+            bbox = anns[i]['bbox']
+            xml += '<object>\n<name>' + str(labelnames[anns[i]['category_id']]) + '</name>\n'
+            xml += '<bndbox>\n<xmin>\n' + str(int(round(bbox[0]))) + '\n</xmin>\n'
+            xml += '<ymin>\n' + str(int(round(bbox[1]))) + '\n</ymin>\n'
+            xml += '<xmax>\n' + str(int(round(bbox[0] + bbox[2]))) + '\n</xmax>\n'
+            xml += '<ymax>\n' + str(int(round(bbox[1] + bbox[3]))) + '\n</ymax>\n</bndbox>\n'
+            xml += '<truncated>\n0\n</truncated>\n<difficult>\n0\n</difficult>\n</object>\n'
+            takeXml=1
     xml += '</annotation>'
-    f_xml = open(directory + img['file_name'].split('.jpg')[0] + '.xml', 'w')
-    f_xml.write(xml)
-    f_xml.close()
+    if(takeXml):
+        f_xml = open(directory + img['file_name'].split('.jpg')[0] + '.xml', 'w')
+        f_xml.write(xml)
+        f_xml.close()
+        takeXml=0
     print (str(n) + ' out of ' + str(len(imgIds)))
